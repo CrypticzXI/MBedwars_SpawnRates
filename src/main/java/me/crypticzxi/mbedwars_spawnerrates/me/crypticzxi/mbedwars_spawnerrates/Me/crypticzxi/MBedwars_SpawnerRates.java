9d71c93fecad0cc03443fbf44f5c20a578965a13
CrypticzXI <plehtide@outlook.com>
2022-04-14 14:55:40 +0100

    Initial commit

---
+++ src/main/java/me/crypticzxi/mbedwars_spawnerrates/me/crypticzxi/mbedwars_spawnerrates/Me/crypticzxi/MBedwars_SpawnerRates.java
@@ -0,0 +1,232 @@
+package me.crypticzxi.mbedwars_spawnerrates.me.crypticzxi.mbedwars_spawnerrates.Me.crypticzxi;
+
+import de.marcely.bedwars.api.arena.Arena;
+import de.marcely.bedwars.api.event.arena.RoundStartEvent;
+import de.marcely.bedwars.api.game.spawner.Spawner;
+import de.marcely.bedwars.api.game.spawner.SpawnerDurationModifier;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+
+public final class MBedwars_SpawnerRates extends JavaPlugin {
+
+    public static JavaPlugin plugin;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getLogger().severe("Shitty Spawn Rates Plugin Has Loaded.");
+        getServer().getPluginManager().registerEvents(new onRoundStartListener(), this);
+        plugin = this;
+        saveConfig();
+        this.saveDefaultConfig();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        Bukkit.getLogger().severe("Shitty Spawn Rates Plugin Has Gone To Bed.");
+        saveConfig();
+    }
+
+    public class onRoundStartListener implements Listener {
+
+        @EventHandler
+        public void onRoundStartEvent(RoundStartEvent event) {
+
+            FileConfiguration config = getConfig();
+
+            List<String> lobby_message = config.getStringList("bedwars-start-message.lines");
+
+            for (String message : lobby_message) {
+                sendCenteredMessage(message);
+            }
+
+            Arena arena = event.getArena();
+            int playersPerTeam = arena.getPlayersPerTeam();
+            double modifier = playersPerTeam/4D + .75D; // solo=1, wingman=1.15, duos=1.25, trios=1.5, quads=1.75
+            for (Spawner spawner : arena.getSpawners()) {
+                spawner.addDropDurationModifier("my_custom_speed", plugin, SpawnerDurationModifier.Operation.MULTIPLY, modifier);
+            }
+            Bukkit.getLogger().info("Yh man");
+        }
+
+    }
+
+
+    private final static int CENTER_PX = 154;
+
+    public static void sendCenteredMessage(String message) {
+        message = ChatColor.translateAlternateColorCodes('&', message);
+
+        int messagePxSize = 0;
+        boolean previousCode = false;
+        boolean isBold = false;
+
+        for (char c : message.toCharArray()) {
+            if (c == '§') {
+                previousCode = true;
+                continue;
+            } else if (previousCode == true) {
+                previousCode = false;
+                if (c == 'l' || c == 'L') {
+                    isBold = true;
+                    continue;
+                } else isBold = false;
+            } else {
+                DefaultFontInfo dFI = DefaultFontInfo.getDefaultFontInfo(c);
+                messagePxSize += isBold ? dFI.getBoldLength() : dFI.getLength();
+                messagePxSize++;
+            }
+        }
+
+        int halvedMessageSize = messagePxSize / 2;
+        int toCompensate = CENTER_PX - halvedMessageSize;
+        int spaceLength = DefaultFontInfo.SPACE.getLength() + 1;
+        int compensated = 0;
+        StringBuilder sb = new StringBuilder();
+        while (compensated < toCompensate) {
+            sb.append(" ");
+            compensated += spaceLength;
+        }
+        for (Player user : Bukkit.getOnlinePlayers()) {
+            user.sendMessage(sb.toString() + message);
+        }
+    }
+
+    enum DefaultFontInfo {
+
+        A('A', 5),
+        a('a', 5),
+        B('B', 5),
+        b('b', 5),
+        C('C', 5),
+        c('c', 5),
+        D('D', 5),
+        d('d', 5),
+        E('E', 5),
+        e('e', 5),
+        F('F', 5),
+        f('f', 4),
+        G('G', 5),
+        g('g', 5),
+        H('H', 5),
+        h('h', 5),
+        I('I', 3),
+        i('i', 1),
+        J('J', 5),
+        j('j', 5),
+        K('K', 5),
+        k('k', 4),
+        L('L', 5),
+        l('l', 1),
+        M('M', 5),
+        m('m', 5),
+        N('N', 5),
+        n('n', 5),
+        O('O', 5),
+        o('o', 5),
+        P('P', 5),
+        p('p', 5),
+        Q('Q', 5),
+        q('q', 5),
+        R('R', 5),
+        r('r', 5),
+        S('S', 5),
+        s('s', 5),
+        T('T', 5),
+        t('t', 4),
+        U('U', 5),
+        u('u', 5),
+        V('V', 5),
+        v('v', 5),
+        W('W', 5),
+        w('w', 5),
+        X('X', 5),
+        x('x', 5),
+        Y('Y', 5),
+        y('y', 5),
+        Z('Z', 5),
+        z('z', 5),
+        NUM_1('1', 5),
+        NUM_2('2', 5),
+        NUM_3('3', 5),
+        NUM_4('4', 5),
+        NUM_5('5', 5),
+        NUM_6('6', 5),
+        NUM_7('7', 5),
+        NUM_8('8', 5),
+        NUM_9('9', 5),
+        NUM_0('0', 5),
+        EXCLAMATION_POINT('!', 1),
+        AT_SYMBOL('@', 6),
+        NUM_SIGN('#', 5),
+        DOLLAR_SIGN('$', 5),
+        PERCENT('%', 5),
+        UP_ARROW('^', 5),
+        AMPERSAND('&', 5),
+        ASTERISK('*', 5),
+        LEFT_PARENTHESIS('(', 4),
+        RIGHT_PERENTHESIS(')', 4),
+        MINUS('-', 5),
+        UNDERSCORE('_', 5),
+        PLUS_SIGN('+', 5),
+        EQUALS_SIGN('=', 5),
+        LEFT_CURL_BRACE('{', 4),
+        RIGHT_CURL_BRACE('}', 4),
+        LEFT_BRACKET('[', 3),
+        RIGHT_BRACKET(']', 3),
+        COLON(':', 1),
+        SEMI_COLON(';', 1),
+        DOUBLE_QUOTE('"', 3),
+        SINGLE_QUOTE('\'', 1),
+        LEFT_ARROW('<', 4),
+        RIGHT_ARROW('>', 4),
+        QUESTION_MARK('?', 5),
+        SLASH('/', 5),
+        BACK_SLASH('\\', 5),
+        LINE('|', 1),
+        TILDE('~', 5),
+        TICK('`', 2),
+        PERIOD('.', 1),
+        COMMA(',', 1),
+        SPACE(' ', 3),
+        DEFAULT('a', 4);
+
+        private char character;
+        private int length;
+
+        DefaultFontInfo(char character, int length) {
+            this.character = character;
+            this.length = length;
+        }
+
+        public char getCharacter() {
+            return this.character;
+        }
+
+        public int getLength() {
+            return this.length;
+        }
+
+        public int getBoldLength() {
+            if (this == DefaultFontInfo.SPACE) return this.getLength();
+            return this.length + 1;
+        }
+
+        public static DefaultFontInfo getDefaultFontInfo(char c) {
+            for (DefaultFontInfo dFI : DefaultFontInfo.values()) {
+                if (dFI.getCharacter() == c) return dFI;
+            }
+            return DefaultFontInfo.DEFAULT;
+        }
+    }
+
+}
